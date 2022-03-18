5f64767a923fb93840079cbca8707d76e567518d
linhphandk <thachlinhphan@gmail.com>
2022-03-18 09:25:52 +0100

    [ADD]: comments

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const { StatusCodes } = require("http-status-codes");
 const mysql = require('mysql');
 const pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : 'db',
+  host            : 'localhost',
+  port: 3306,
+  insecureAuth : true,
   user            : 'root',
-  password        : 'example',
+  password        : 'rootroot',
   database        : 'ExpressExample'
 });
 
 
 const app = express()
-app.use(express.json());
+app.use(express.json());// instentiate the middleware, and we do it because usually the body is empty because the request data is saved somewhere else, spthis takes it and converts it into json and puts it into the body 
 
+//defien endpoint
 app.post('/user', (req,res)=>{
     const {username, password} = req.body
     if(username === undefined){
-        res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY) // in case we miss the username, it would crassh if we don't have this
         res.send("Missing param username")
         return
     }
@@ -26,12 +29,14 @@ app.post('/user', (req,res)=>{
         res.send("Missing param password")
         return
     }
+    // in general with mysql we connect and need to disconent and pool manages the connection
     pool.query(
-        "INSERT INTO User (username, password) VALUES (?,?)",
+        "INSERT INTO User (username, password) VALUES (?,?)", //? means that we parmaterise the values (username nad password) so the ? are just placeholders
+        // missing the hashing - Linh will add it later 😛 
         [username,password],
-        function (error, results, fields) {
+        function (error, results, fields) { // here is the fuction where we return the new id
             if (error) throw error;
-            res.status(StatusCodes.CREATED)
+            res.status(StatusCodes.CREATED) // change the status code to 201, because by defalt we will get 200 and 201 for creating
             res.send({id:results.insertId})
         }
     )
@@ -53,6 +58,7 @@ app.get('/authenticate', (req,res)=>{
         "Select (id) from User where username=? and password=?",
         [username,password],
         function (error, results, fields) {
+            // missing the token when returing the value! Linh will fix it
             if (error) throw error;
             res.status(StatusCodes.OK)
             res.send({id:results.insertId})
@@ -61,6 +67,6 @@ app.get('/authenticate', (req,res)=>{
 })
 
 
-app.listen(8000,()=>{
+app.listen(8000,()=>{ // this is how we start the Express server
     console.log("starting")
 })
